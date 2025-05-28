b8d53757c0ac40197834280fcc02e1dee9987bc6
Shaharyar Ahmed <113702415+Shaharyar2310@users.noreply.github.com>
2025-05-29 00:38:50 +0400

    .
    .

--- src/components/index.js
+++
@@ -1,7 +0,0 @@
-
-export { default as MovieExplorer } from './MovieExplorer.jsx';
-export { default as BookExplorer } from './BookExplorer.jsx';
-export { default as Navigation } from './Navigation.jsx';
-export { default as Footer } from './Footer.jsx';
-export { default as Modal } from './Modal.jsx';
-export { default as App } from '../App.jsx';
